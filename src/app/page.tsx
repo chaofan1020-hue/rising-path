49ba21cde0008a346ce4c01d5a50b49e5fe3c9c5
user4975083135 <2948250361413193-user4975083135@noreply.coze.cn>
2026-04-08 13:43:31 +0800

    style: 调整岗位统计组件位置

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -194,7 +194,7 @@ export default function Home() {
       </section>
 
       {/* Job Stats Section */}
-      <section className="container mx-auto px-4 pt-8 pb-2">
+      <section className="container mx-auto px-4 pt-4 pb-2">
         <div className="max-w-xl mx-auto">
           <JobStatsWidget />
         </div>
