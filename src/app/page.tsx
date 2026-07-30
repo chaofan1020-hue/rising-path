f179887e276b933b1cffebc2699ee606e6334acc
user4975083135 <2948250361413193-user4975083135@noreply.coze.cn>
2026-07-31 00:02:32 +0800

    feat: Footer 固定在视口底部
    Coze-Commit-Type: user
    Coze-User-ID: 2948250361413193
    Coze-Conversation-ID: 2875443

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,22 @@ import { Footer } from '@/components/ui/footer';
 
 export default function Home() {
   return (
-    <div className="min-h-screen flex flex-col bg-white">
-      <div className="flex-1">
-        <FeaturesCards />
-        <FAQs />
+    <div className="min-h-screen bg-white">
+      <FeaturesCards />
+      <FAQs />
+      <div className="fixed bottom-0 left-0 right-0 z-50">
+        <Footer
+          logo={<img src="/logo.svg" alt="Rising Path" className="h-8 w-8" />}
+          brandName="Rising Path"
+          socialLinks={[]}
+          mainLinks={[]}
+          legalLinks={[]}
+          copyright={{
+            text: "© 2026 Rising Path",
+            license: "All rights reserved",
+          }}
+        />
       </div>
-      <Footer
-        logo={<img src="/logo.svg" alt="Rising Path" className="h-8 w-8" />}
-        brandName="Rising Path"
-        socialLinks={[]}
-        mainLinks={[]}
-        legalLinks={[]}
-        copyright={{
-          text: "© 2026 Rising Path",
-          license: "All rights reserved",
-        }}
-      />
     </div>
   );
 }
