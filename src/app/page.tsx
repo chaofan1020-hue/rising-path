37d370bca4d1ad877bf657e5a06e09a75e6cbcf4
user4975083135 <2948250361413193-user4975083135@noreply.coze.cn>
2026-07-31 21:03:16 +0800

    auto saved your changes before restore
    Coze-Commit-Type: auto
    Coze-User-ID: 2948250361413193

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,28 +3,16 @@
 import FeaturesCards from '@/components/features-cards';
 import FAQs from '@/components/faqs-component';
 import { Header1 } from '@/components/header1';
-import AboutSection3 from '@/components/about-section';
 import { useLanguage } from '@/lib/language-context';
-import { useRef } from 'react';
 
 export default function Home() {
   const { t } = useLanguage();
-  const featuresRef = useRef<HTMLDivElement>(null);
-
-  const scrollToFeatures = () => {
-    featuresRef.current?.scrollIntoView({ behavior: 'smooth' });
-  };
 
   return (
     <div className="min-h-screen bg-white dark:bg-black">
       <Header1 />
       <div className="pt-20">
-        <div className="min-h-screen flex items-center justify-center">
-          <AboutSection3 onStartClick={scrollToFeatures} />
-        </div>
-        <div ref={featuresRef}>
-          <FeaturesCards />
-        </div>
+        <FeaturesCards />
         
         {/* Platform Introduction */}
         <section className="py-16 px-4 bg-white dark:bg-black">
