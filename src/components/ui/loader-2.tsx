5fc2806d2d74d80a9ad5dde4400a85b61cdd8b2e
user4975083135 <2948250361413193-user4975083135@noreply.coze.cn>
2026-08-01 10:30:07 +0800

    style(loader): 动画中的点改为正红色 #FF0000
    Coze-Commit-Type: user
    Coze-User-ID: 2948250361413193
    Coze-Conversation-ID: 2875443

--- src/components/ui/loader-2.tsx
+++ src/components/ui/loader-2.tsx
@@ -8,7 +8,7 @@ export const Loader2 = () => {
       <style jsx>{`
         .loader {
           --path: currentColor;
-          --dot: #C46A4A;
+          --dot: #FF0000;
           --duration: 3s;
           width: 44px;
           height: 44px;
